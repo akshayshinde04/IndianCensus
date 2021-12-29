dbcc9d675a4ec3c520f552f8a3b598f20a2b3b2f
unknown <akshayHVshinde04@gmail.com>
2021-12-29 15:30:47 +0530

    File if Incorrect adding Custom Exception

---
+++ src/com/exception/InvalidFileName.java
@@ -0,0 +1,10 @@
+package com.exception;
+
+public class InvalidFileName extends Exception{
+
+	public InvalidFileName(String string) {
+		super(string);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
--- src/com/test/StateCensusAnalyserTest.java
+++ src/com/test/StateCensusAnalyserTest.java
@@ -7,10 +7,20 @@ import org.junit.Test;
 import com.main.StateCensusAnalyser;
 
 public class StateCensusAnalyserTest {
+	StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
 	@Test
 	public void test_reading_CSVdata() throws Exception {
-		StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-		censusAnalyser.readCSVdata();
+		censusAnalyser.readCSVdata("C:\\Users\\Akshay Shinde\\eclipse-workspace\\IndianCensusAnalyser\\src\\com\\resources\\IndiaStateCensusData.csv");
 		assertEquals(true, censusAnalyser.countEntries());
 	}
-}
+	@Test
+	public void testInvalidFilename() throws Exception {
+		try {
+			censusAnalyser.readCSVdata("C:\\Users\\Akshay Shinde\\eclipse-workspace\\IndianCensusAnalyser\\src\\com\\resources\\abcIndiaStateCensus.csv");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+} 
